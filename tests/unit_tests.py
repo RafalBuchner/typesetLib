1e9d8a5a716ab796064e574eca009d52328f2ca4
rafal <rafal.buchner@gmail.com>
2023-07-02 23:33:51 +0200

    unitest fix

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -9,19 +9,19 @@ article = open("article.txt", "r").read()
 class AbstractObjectsTest(unittest.TestCase):
     def test_text(self):
         from typesetLib.abstract.text import TextObj
-        from typesetLib.abstract.styles import AbstractCharacterStyle
+        from typesetLib.abstract.styles import CharacterStyle
         txt = TextObj()
         self.assertIsNone(txt.getText())
         self.assertIsNone(txt.paragraphs)
 
         txt.setText(article)
         self.assertListEqual(txt.textSegments, [])
-        segment_0 = txt.setCharacterStyle(0, 3, 5, characterStyle=AbstractCharacterStyle())
+        segment_0 = txt.setCharacterStyle(0, 3, 5, characterStyle=CharacterStyle())
 
         self.assertEqual(len(txt.textSegments), 1)
         self.assertEqual(txt.textSegments[0].txtStr, "2017,")
 
-        segment_1 = txt.setCharacterStyle(0, segment_0.endIndex, 8, characterStyle=AbstractCharacterStyle())
+        segment_1 = txt.setCharacterStyle(0, segment_0.endIndex, 8, characterStyle=CharacterStyle())
         self.assertEqual(len(txt.textSegments), 2)
 
         textMap, styleMap = txt.paragraphs[0].getTextAndStyleMaps()
