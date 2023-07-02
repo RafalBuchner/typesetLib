0507aec81f3ff171a71346f99a4cffbfb0e46fe2
rafal <rafal.buchner@gmail.com>
2023-07-02 23:03:01 +0200

    created layout basic object

--- typesetLib/abstract/basic.py
+++ typesetLib/abstract/basic.py
@@ -8,4 +8,25 @@ class ProofBasicObject(object):
 	def getUserData(self, key):
 		return self.userData[key]
 
+class LayoutBasicObject(ProofBasicObject):
+	def __init__(self, index, parent=None):
+		self.index = index
+		self.parent = parent
+		self.children = []
+
+	def setParent(self, obj):
+		if obj not is None:
+			obj.children.append(self)
+		self.parent = obj
+
+	def addChild(self, obj):
+		obj.setParent(self)
+
+	def indexUP(self):
+		self.index += 1
+
+	def indexDOWN(self):
+		NotImplemented
+
+
 		
