21e635dbcffada66321cd97b5dc537c04aef64e8
rafal <rafal.buchner@gmail.com>
2023-07-02 22:26:32 +0200

    update unitest

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -3,91 +3,33 @@ import os
 import pprint
 import sys
 
-
 article = open("article.txt", "r").read()
 
 
 class AbstractObjectsTest(unittest.TestCase):
     def test_text(self):
         from typesetLib.abstract.text import AbstractText
+        from typesetLib.abstract.styles import AbstractCharacterStyle
         txt = AbstractText()
-        # self.assertIsNotNone(ruleConditionDesc)
+        self.assertIsNone(txt.getText())
+        self.assertIsNone(txt.paragraphs)
 
-        # self.assertIsNone(ruleConditionDesc.name)
-        # ruleConditionDesc.setName("Optical Size")
-        # self.assertIsNotNone(ruleConditionDesc.name)
+        txt.setText(article)
+        self.assertListEqual(txt.textSegments, [])
+        segment_0 = txt.setCharacterStyle(0, 3, 5, characterStyle=AbstractCharacterStyle())
 
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleConditionDesc.setName(2.3)
+        self.assertEqual(len(txt.textSegments), 1)
+        self.assertEqual(txt.textSegments[0].txtStr, "2017,")
 
-        # self.assertIsNone(ruleConditionDesc.minimum)
-        # ruleConditionDesc.setMinimum(8)
-        # self.assertIsNotNone(ruleConditionDesc.minimum)
+        segment_1 = txt.setCharacterStyle(0, segment_0.endIndex, 8, characterStyle=AbstractCharacterStyle())
+        self.assertEqual(len(txt.textSegments), 2)
 
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleConditionDesc.setMinimum("Optical Size")
+        textMap, styleMap = txt.paragraphs[0].getTextAndStyleMaps()
+        self.assertEqual(len(textMap), len(styleMap))
 
-        # self.assertIsNone(ruleConditionDesc.maximum)
-        # ruleConditionDesc.setMaximum(28)
-        # self.assertIsNotNone(ruleConditionDesc.maximum)
+        textMap, styleMap = txt.getTextAndStyleMaps()
+        self.assertEqual(len(textMap), len(styleMap))
 
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleConditionDesc.setMaximum("Optical Size")
-
-        # ruleEntryDesc = RuleEntryDescriptor()
-        # self.assertIsNotNone(ruleConditionDesc)
-
-        # self.assertIsNone(ruleEntryDesc.glyphSuffix)
-        # ruleEntryDesc.setGlyphSuffix(".rule")
-        # self.assertIsNotNone(ruleEntryDesc.glyphSuffix)
-
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleEntryDesc.setGlyphSuffix(3213)
-
-        # self.assertIsNone(ruleEntryDesc.ruleName)
-        # ruleEntryDesc.setRuleName("opsz_alt")
-        # self.assertIsNotNone(ruleEntryDesc.ruleName)
-
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleEntryDesc.setRuleName(dict())
-
-        # self.assertIsInstance(ruleEntryDesc.conditionSet, list)
-        # self.assertEqual(
-        #         len(ruleEntryDesc.conditionSet), 0
-        #     )
-        # ruleEntryDesc.setConditionSet([ruleConditionDesc])
-        # self.assertNotEqual(len(list(ruleEntryDesc.conditionSet)), 0)
-
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleEntryDesc.setConditionSet(["lala"])
-
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleEntryDesc.setConditionSet({"lala":23})
-
-        # ruleEntryDesc.setConditionSet([])
-        # self.assertEqual(
-        #         len(ruleEntryDesc.conditionSet), 0
-        #     )
-        # ruleEntryDesc.addRuleCondition(ruleConditionDesc)
-        # self.assertNotEqual(len(list(ruleEntryDesc.conditionSet)), 0)
-
-        # with self.assertRaises(AssertionError) as context:
-        #     ruleEntryDesc.addRuleCondition(111111)
-
-
-        # rulesDesc = RulesDescriptor()
-        # self.assertIsNotNone(rulesDesc)
-
-
-        # self.assertEqual(len(rulesDesc.rules.keys()), 0)
-        # rulesDesc.setRules({ruleEntryDesc.ruleName:ruleEntryDesc})
-        # self.assertNotEqual(len(list(rulesDesc.rules.keys())), 0)
-        
-        # rulesDesc.setRules({})
-        # self.assertEqual(len(list(rulesDesc.rules.keys())), 0)
-
-        
 
 if __name__ == "__main__":
-    
     unittest.main()
