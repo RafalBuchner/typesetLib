5663477a6211138581ddfa8e28532c48c5be3665
rafal <rafal.buchner@gmail.com>
2023-07-05 00:32:38 +0200

    page is better

--- typesetLib/abstract/page.py
+++ typesetLib/abstract/page.py
@@ -2,10 +2,40 @@ from typesetLib.abstract.basic import LayoutBasicObject
 
 
 class Page(LayoutBasicObject):
-	def __int__(self, index, size, startPgNumber=1):
-		super().__int__(index, size)
-		self.startPgNumber = startPgNumber
+	def __init__(self, size, index=0, startPgNumber=1):
+		super().__init__(size, index)
+		self.setStartingPgNumber(startPgNumber)
 
 	@property
 	def currentPgNumber(self):
 		return self.startPgNumber + self.index
+
+	def pageFlip(self):
+		"""
+		means, that page has changed by 1
+		"""
+		self.index += 1
+
+	def resetPgNumberToStart(self):
+		self.setCurrentPgNumber(self.startPgNumber)
+
+	def setStartingPgNumber(self, pgNumber):
+		self.startPgNumber = pgNumber
+
+	def setCurrentPgNumber(self, pgNumber):
+		self.index = pgNumber - self.startPgNumber
+
+	def setIndex(self, value):
+		self.index = value
+
+
+
+	# not supported inherented methods:
+	def setParent(self, obj):
+		NotImplemented
+
+	def setPosition(self, obj):
+		NotImplemented
+
+	def getPosition(self, obj):
+		NotImplemented
