022adf952b993558e74a410eeb5ed51a440ab38b
rafal <rafal.buchner@gmail.com>
2023-07-05 00:32:46 +0200

    story is better

--- typesetLib/abstract/story.py
+++ typesetLib/abstract/story.py
@@ -1,14 +1,24 @@
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from typesetLib.abstract.text import TextObj
+    from typesetLib.abstract.layoutTextBox import LayoutTextBox
+
 from typesetLib.abstract.basic import ProofBasicObject
 
 
+
+
 class Story(ProofBasicObject):
-	def __init__(self, textObj):
+	def __init__(self, textObj: TextObj):
 		self.textObj = textObj
 		self.textBoxes = []
 
+		self.textReminds: TextObj
 
-	def addTextBox(self, layoutTextBox):
-		self.textBoxes.assignToStory(self)
+
+	def addTextBox(self, layoutTextBox: LayoutTextBox):
+		self.textBoxes.append(layoutTextBox)
 
 
 
