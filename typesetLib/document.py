830b272ba614c5f8ff09f2946a847cb026db34af
rafal <rafal.buchner@gmail.com>
2023-07-09 22:00:18 +0200

    Document class init

---
+++ typesetLib/document.py
@@ -0,0 +1,13 @@
+
+class Document:
+    def __init__(self):
+        NotImplemented
+
+    def addMasterPage(self, pageName, page):
+        NotImplemented
+
+    def addPage(self, pageName, customStartingPageNumber=None):
+        NotImplemented
+
+    def exportDrawbot(self, path):
+        NotImplemented
